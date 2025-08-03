dd68da10973b1d908674d608bb0965549a5a9ee7
Chenhao Wu <38174336+wuchenhaoo@users.noreply.github.com>
2025-08-04 00:36:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     add_argument('--zotero_id', type=str, help='Zotero user ID')
     add_argument('--zotero_key', type=str, help='Zotero API key')
     add_argument('--zotero_ignore',type=str,help='Zotero collection to ignore, using gitignore-style pattern.')
-    add_argument('--send_empty', type=bool, help='If get no arxiv paper, send empty email',default=False)
+    add_argument('--send_empty', type=bool, help='If get no arxiv paper, send empty email',default=True)
     add_argument('--max_paper_num', type=int, help='Maximum number of papers to recommend',default=100)
     add_argument('--arxiv_query', type=str, help='Arxiv search query')
     add_argument('--smtp_server', type=str, help='SMTP server')
